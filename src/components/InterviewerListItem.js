bb1ffb7742f9366649fba83bcef0f40fe0ec4fa7
Brian Duong <brianminhduong@gmail.com>
2022-02-22 19:18:24 -0500

    Create InterviewerListItem component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classNames from "classnames";
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+  const interviewerClasses = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
+  const showName = props.selected ? props.name : "";
+  return (
+    <li className={interviewerClasses} onClick={props.setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {showName}
+    </li>
+  );
+}
